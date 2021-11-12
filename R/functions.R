3af178290d1e42ec0af05be87bd52650fcfca458
Natalie Gray <58091874+natmaegray@users.noreply.github.com>
2021-11-11 21:43:06 -0700

    getting trip generation to work

--- R/functions.R
+++ R/functions.R
@@ -5,24 +5,82 @@
 
 #read skims
 readskims <- function(path){
-  read_all_omx(path)
+  read_all_omx(path) %>%
+    mutate(auto = SOV_TIME__AM, nonmotor = WLK_LOC_WLK_TOTIVT__AM/100 , transit = WLK_COM_WLK_TOTIVT__AM/100 ) %>%
+    select(c("origin", "destination","DIST", "auto", "nonmotor", "transit"))
 }
 
 #read CSVs
-
-# read in SE DATA, read in trip rates, read in mc / dc utilities
 readCSV <- function(path){
   read_csv(path)
 }
 
+#organize hh data
+hh_data <- function(households){
+  households %>% 
+    select(HHID, income, PERSONS, VEHICL, workers) %>%
+    mutate(
+      hhsize = ifelse(PERSONS > 4, 4, PERSONS) ,
+      hhvehcnt = ifelse(VEHICL > 3, 3, VEHICL),
+      wrkcount = ifelse(workers > 2, 2, workers)
+    ) %>%
+    select(HHID, income, hhsize, hhvehcnt, wrkcount)
+}
 
-# run trip gen
+#use nhts2017 data to get approximate trip productions
+nhts_2017 <- function(nhts_households, nhts_trips){
+  hh <- nhts_households %>% 
+    filter(msasize == "02", !travday %in% c("01", "07")) %>%
+    select(houseid, wthhfin, hhsize, hhvehcnt, numadlt, hhfaminc, wrkcount) %>%
+    mutate(
+      hhsize = ifelse(hhsize > 4, 4, hhsize),
+      hhvehcnt = ifelse(hhvehcnt > 3, 3, hhvehcnt),
+      wrkcount = ifelse(wrkcount > 2, 2, wrkcount)
+    )
+  trips <- nhts_trips %>% 
+    filter(houseid %in% hh$houseid) %>%
+    group_by(houseid, trippurp) %>%
+    summarise(trips = n()) %>%
+    pivot_wider(id_cols = houseid, names_from = trippurp, 
+                values_from = trips, values_fill = 0)
+  nato0 <- function(x) {ifelse(is.na(x), 0, x)}
+  nhts_tripprod <- hh %>% 
+    left_join(trips, by = "houseid") %>%
+    mutate_at(vars(names(trips)), nato0) %>%
+    select(c("hhsize", "hhvehcnt", "numadlt", "wthhfin", "wrkcount", "HBW","HBO", "NHB")) %>%
+    group_by(hhsize, hhvehcnt, wrkcount) %>%
+    summarise(HBW = mean(HBW),
+              HBO = mean(HBO),
+              NHB = mean(NHB))
+  nhts_tripprod
+}
+
+#trip gen
+tripprod <- function(households, nhts_tripprod){
+  nato0 <- function(x) {ifelse(is.na(x), 0, x)}
+  
+  nhts_tripprod2 <- nhts_tripprod %>% 
+    mutate("svw" = paste(hhsize, hhvehcnt, wrkcount))
+  
+  households2 <- households %>% 
+    mutate("svw" = paste(hhsize, hhvehcnt, wrkcount)) %>%
+    left_join(nhts_tripprod2, by = "svw") %>%
+    select(-c("hhsize.y", "hhvehcnt.y", "wrkcount.y"))
+  
+  households2
+}
+
+
+
 
 
 # run mode choice logsum calculator
-
+mc_logsum <- function(){
+  #  HBW_util = 
+  #  HBO_util = 
+  #  NHB_util = 
+}
 
 # run destination choice calculator / compute destination choice
-
 
 # compute mode choice probability
--- _targets.R
+++ _targets.R
@@ -8,7 +8,7 @@ library(targets)
 # and tar_read(summary) to view the results.
 
 # Set target-specific options such as packages.
-tar_option_set(packages = c("tidyverse", "bookdown", "omxr"))
+tar_option_set(packages = c("tidyverse", "bookdown", "omxr", "nhts2017"))
 
 # Define custom functions and other global objects.
 # This is where you write source(\"R/functions.R\")
@@ -22,13 +22,16 @@ data_targets <- list(
   tar_target(skims_file, "data/skims.omx", format = "file"),
   tar_target(skims, readskims(skims_file)),
   
-  # read in SE DATA
-  tar_target(se_file, "data/households.csv", format = "file"), #need right file input
-  tar_target(se_data, readCSV(se_file)),
-  
-  # read in trip rates
-  tar_target(triprates_file, "data/households.csv", format = "file"), #need right file input
-  tar_target(trip_rates, readCSV(triprates_file)),
+  # household DATA
+  tar_target(hh_file, "data/households.csv", format = "file"), 
+  tar_target(hh, readCSV(hh_file)),
+  tar_target(hh_clean, hh_data(hh)),
+
+  # trip rates
+  tar_target(nhts_hh, nhts_households),
+  tar_target(nhts_trip, nhts_trips),
+  tar_target(nhts_tripprod, nhts_2017(nhts_hh, nhts_trip)),
+  tar_target(trips, tripprod(hh_clean, nhts_tripprod)),
   
   # read in mc / dc utilities
   tar_target(mc_coeff_file, "data/MC_coeff.csv", format = "file"),
@@ -39,6 +42,7 @@ data_targets <- list(
   tar_target(dc_param, readCSV(dc_param_file))
   
   # run trip gen
+  
   
   # run mode choice logsum calculator
   
